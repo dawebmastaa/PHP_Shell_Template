2a03eac767b585f4949fea63f1813f272cd6cd4e
Mike Alberts <mikealberts@gmail.com>
2023-01-10 16:08:21 -0500

    Fixed static home page.

--- web/functions/buildstatiichome.php
+++ web/functions/buildstatiichome.php
@@ -1,14 +1,22 @@
 <?php
-//check for the home page and delete it
-if(file_exists($ApplicationPath.'/index.html')) {
-    unlink($ApplicationPath . '/index.html');
-    clearstatcache();
-}
-else
+//initialize the variables needed for this page.
+$VariableArray = array('rebuild');
+
+//pass array to getvariables function to copy their values from URL variables to local scope (if they exist)
+GetVariables($VariableArray);
+
+if(isset($rebuild) && $rebuild = 'Y')
 {
-    //make a new one
-    $StaticPage = file_get_contents($root.'main/');
-    //file_put_contents($ApplicationPath.'/index.html', fopen("$root".'main/index.php', 'r'));
-    file_put_contents($ApplicationPath.'/index.html',$StaticPage, LOCK_EX);
+    //check for the home page, delete it, then make a new one.
+    if(file_exists($ApplicationPath.'/index.html'))
+    {
+        unlink($ApplicationPath .'/index.html');
+        clearstatcache();
+    }
+    elseif(file_exists($ApplicationPath.'/main/cache/main.html'))
+    {
+        $StaticPage = file_get_contents($ApplicationPath.'/main/cache/main.html');
+        file_put_contents($ApplicationPath.'/index.html',$StaticPage, LOCK_EX);
+    }
 }
 ?>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
 <?php
 include_once('functions/buildstatiichome.php');
-//everything at site root runs out of the 'main' directory
-include_once('main/index.php');
+
+if(file_exists($ApplicationPath.'/index.html'))
+{
+    include_once('index.html');
+    die();
+}
+else
+{
+    //everything at site root runs out of the 'main' directory
+    include_once('main/index.php');
+}
 ?>
